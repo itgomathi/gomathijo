5e9538dad97a6124cc7c9de18cb0f10d1b585568
itgomathi <gomathikrishnan157@gmail.com>
2016-06-06 21:58:19 -0700

    Create gomathijo.c

---
+++ gomathijo.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+int n;
+scanf("%d",&n);
+if(n<0)
+{
+printf("the number is negative");
+}
+elseif(n>0)
+{
+printf("the number is pastive");
+}
+else
+{
+printf("the number is zero");
+}
+}
